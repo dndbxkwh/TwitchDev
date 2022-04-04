880f827cea91eb1ff5217b293eabc5ca8e2d94d6
dndbxkwh <dndbxkwh@gmail.com>
2022-04-04 18:41:18 +0900

    ver0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
   name="TwitchDev",
-  version="0.0.1",
+  version="0.0.2",
   author="dndbxkwh",
   author_email="dndbxkwh@gmail.com",
   description="TwitchDev",
